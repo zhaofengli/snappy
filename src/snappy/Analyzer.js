fc9f74a8ae064e62c8b623d0d1a49a11e75dacf5
Zhaofeng Li <hello@zhaofeng.li>
2017-07-28 13:10:32 -0700

    snappy/Analyzer.js: Add default parameter for options

--- src/snappy/Analyzer.js
+++ src/snappy/Analyzer.js
@@ -1,7 +1,12 @@
 import AnalyzerWorker from '@/workers/Analyzer';
 
+const defaultOptions = {
+  enableGenosets: true,
+  enableExtraSnpInfo: false,
+};
+
 export default class Analyzer {
-  static analyze(file, options) {
+  static analyze(file, options = defaultOptions) {
     return new Promise((resolve, reject) => {
       const worker = new AnalyzerWorker();
       worker.onmessage = (event) => {
