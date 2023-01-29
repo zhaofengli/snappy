acaa3245edf680ec58d5ae51019d57d07085ac58
Jack Britchford <jackbritchford@users.noreply.github.com>
2023-01-29 03:57:17 +0000

    messing with gh actions stuff

---
+++ docker-node/docker-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+
+## This is from the node repo: https://raw.githubusercontent.com/nodejs/docker-node/main/docker-entrypoint.sh
+
+# Run command with node if the first argument contains a "-" or is not a system command. The last
+# part inside the "{}" is a workaround for the following bug in ash/dash:
+# https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=874264
+if [ "${1#-}" != "${1}" ] || [ -z "$(command -v "${1}")" ] || { [ -f "${1}" ] && ! [ -x "${1}" ]; }; then
+  set -- node "$@"
+fi
+
+exec "$@"
